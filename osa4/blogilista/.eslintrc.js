0187564ee3e1319212b49bfd0bd301e396823da6
ollinok <62589655+ollinok@users.noreply.github.com>
2022-05-25 14:24:08 +0300

    osa4 tehtävät 4.1 - 4.2

---
+++ osa4/blogilista/.eslintrc.js
@@ -0,0 +1,47 @@
+/* eslint-disable indent */
+module.exports = {
+    'env': {
+        'browser': true,
+        'commonjs': true,
+        'es2021': true
+    },
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 'latest'
+    },
+    'globals': {
+        'process': 'readonly'
+    },
+    'rules': {
+        'indent': [
+            'error',
+            2
+        ],
+        'linebreak-style': [
+            'error',
+            'windows'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        'eqeqeq': [
+            'error'
+        ],
+        'no-trailing-spaces': [
+            'error'
+        ],
+        'object-curly-spacing': [
+            'error',
+            'always'
+        ],
+        'arrow-spacing': [
+            'error',
+            { 'before': true, 'after': true }
+        ]
+    }
+};
---
+++ osa4/blogilista/app.js
@@ -0,0 +1,22 @@
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const blogsRouter = require('./controllers/blogs');
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('Connected to the database.');
+  }).catch(err => {
+    logger.error(err);
+  });
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/blogs', blogsRouter);
+
+module.exports = app;
